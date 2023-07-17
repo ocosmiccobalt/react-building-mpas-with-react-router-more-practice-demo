21de05e0430dee933d1c0d20c293983115fe8b56
Olga Bazhenova <ocosmiccobalt@gmail.com>
2023-07-18 01:09:14 +0300

    Output a list of dummy events to the EventsPage

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@
 // DONE
 // 6. Output a list of dummy events to the EventsPage
 //    Every list item should include a link to the respective EventDetailPage
+// DONE
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
