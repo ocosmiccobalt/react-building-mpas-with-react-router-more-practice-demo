fb20b6e79ffbd7417782eca7fd752a6687a5a226
Olga Bazhenova <ocosmiccobalt@gmail.com>
2023-07-21 16:04:17 +0300

    Extract error data & throw responses

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -25,8 +25,10 @@ export const loader = async () => {
 
   if (!response.ok) {
     // return { isError: true, message: 'Could not fetch events.' };
-    // eslint-disable-next-line
-    throw { message: 'Could not fetch events.' };
+    throw new Response(
+      JSON.stringify({ message: 'Could not fetch events.' }),
+      { status: 500 }
+    );
   } else {
     return response;
   }
