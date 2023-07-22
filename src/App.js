53d3ae5509ecd10de38fd6a86ca12c698530da53
Olga Bazhenova <ocosmiccobalt@gmail.com>
2023-07-22 22:00:45 +0300

    Submit data programmatically with useSubmit() hook

--- src/App.js
+++ src/App.js
@@ -32,7 +32,10 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import HomePage from './pages/Home';
 import EventsPage, { loader as eventsLoader } from './pages/Events';
-import EventDetailPage, { loader as eventDetailLoader } from './pages/EventDetail';
+import EventDetailPage, {
+  loader as eventDetailLoader,
+  action as deleteEventAction
+} from './pages/EventDetail';
 import NewEventPage, { action as newEventAction } from './pages/NewEvent';
 import EditEventPage from './pages/EditEvent';
 import RootLayout from './pages/Root';
@@ -60,7 +63,11 @@ const router = createBrowserRouter([
             id: 'event-detail',
             loader: eventDetailLoader,
             children: [
-              { index: true, element: <EventDetailPage /> },
+              {
+                index: true,
+                element: <EventDetailPage />,
+                action: deleteEventAction
+              },
               { path: 'edit', element: <EditEventPage /> }
             ]
           },
